051d9df70fd32551d2f0cc6d8d14148110ff082e
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2024-06-27 02:27:35 -0400

    Add files via upload

--- qbdl_gui.py
+++ qbdl_gui.py
@@ -13,8 +13,6 @@ app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(24)
 encryption_key = os.environ.get('ENCRYPTION_KEY') or Fernet.generate_key()
 fernet = Fernet(encryption_key)
 
-qobuz_dl = None  # Initialize QobuzDL instance globally
-
 def encrypt_password(password):
     return fernet.encrypt(password.encode()).decode()
 
@@ -23,8 +21,6 @@ def decrypt_password(encrypted_password):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global qobuz_dl
-
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -32,20 +28,15 @@ def index():
         download_location = request.form['download_location']
         quality = int(request.form['quality'])
         remember = request.form.get('rememberMe')
-        search_type = request.form.get('searchType')  # Get search type from form
 
         try:
-            qobuz_dl = QobuzDL(
+            qobuz = QobuzDL(
                 directory=download_location,
                 quality=quality
             )
-            qobuz_dl.get_tokens()
-            qobuz_dl.initialize_client(email, password, qobuz_dl.app_id, qobuz_dl.secrets)
-            # Handle search based on type
-            results = qobuz_dl.search_by_type(url, search_type, limit=10)
-            return render_template('index.html', email=email, password=password,
-                                   download_location=download_location, quality=quality,
-                                   search_results=results)
+            qobuz.get_tokens()
+            qobuz.initialize_client(email, password, qobuz.app_id, qobuz.secrets)
+            qobuz.handle_url(url)
         except Exception as e:
             logging.error("An error occurred: " + str(e))
             return jsonify(status='error', message='An internal error occurred. Please try again later.'), 500
@@ -66,20 +57,6 @@ def index():
 
     return render_template('index.html', email=email, password=password, download_location=download_location, quality=quality)
 
-# Route to handle AJAX search requests
-@app.route('/search', methods=['GET'])
-def search():
-    global qobuz_dl
-
-    query = request.args.get('query')
-    search_type = request.args.get('searchType')
-
-    if query and search_type and qobuz_dl:
-        try:
-            results = qobuz_dl.search_by_type(query, search_type, limit=10)
-            return jsonify(results)
-        except Exception as e:
-            logging.error(f"An error occurred during search: {str(e)}")
-            return jsonify([])  # Return empty list on error
-
-    return jsonify([])  # Return empty list if no query or search type provided
+#if __name__ == '__main__':
+    #from werkzeug.serving import run_simple
+    #run_simple('0.0.0.0', 5000, app)
